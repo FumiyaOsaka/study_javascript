91620ec3d19f53bc023ca8478628851b9214c002
NatsumiAsami <lia27.sqt@gmail.com>
2023-12-01 11:23:48 +0900

    J-2 課題提出

---
+++ js/j-2.js
@@ -0,0 +1,15 @@
+//constは「定数」を宣言するときのキーワードです。
+const JS_TEXT = "これは外部javascriptファイルで定義した定数です。";
+
+//letは「変数」を宣言するときのキーワードです。
+let str1 = "これは";
+
+let str2 = "外部javascriptファイルで定義した";
+
+let str3 = "変数です。";
+
+//文字列同士を結合したい場合は"+"を使います。
+let var_text = str1 + str2 + str3;
+
+//console.logは、このjavascriptが実行された環境に文字を出力する命令文です。
+console.log(var_text);
